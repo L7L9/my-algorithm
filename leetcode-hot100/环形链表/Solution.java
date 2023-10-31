be20d9e763c1a99f64294b96fa5f4eea9b618001
lql <1964757607@qq.com>
2023-10-31 10:56:52 +0800

    feat:添加[环形链表]的题解

--- leetcode-hot100/环形链表/Solution.java
+++ leetcode-hot100/环形链表/Solution.java
@@ -10,6 +10,11 @@
  * }
  */
 public class Solution {
+    /**
+     * 给你一个链表的头节点 head ，判断链表中是否有环。
+     * 如果链表中有某个节点，可以通过连续跟踪 next 指针再次到达，则链表中存在环。
+     * 如果链表中存在环，则返回 true。否则，返回 false。
+     */
     public boolean hasCycle(ListNode head) {
         if(head == null || head.next == null){
             return false;
